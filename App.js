9b512bf9630215c0d655d91b7fd5ed02de52c7d5
Muhammad Miram <139574757+miraameditz@users.noreply.github.com>
2024-09-21 03:04:29 +0500

    Update App.js

--- App.js
+++ App.js
@@ -4,15 +4,14 @@ var PopUp = document.getElementById("PopUp");
 var arr = [];
 
 function login() {
-    arr.push(userName.value.toLowerCase);
-    userName.value = "";
+    arr.push(userName.value);
 }
 
 function checkName() {
-    if (arr.includes(input.value.toLowerCase)) {
+    if (arr.includes(input.value)) {
         PopUp.innerHTML = `<div class="VerifyForm"> <i class="fa-solid fa-check green"></i> <br /> You Are Now Verified </div>`;
     } else {
         PopUp.innerHTML = `<div class="VerifyForm"> <i class="fa-solid fa-x red"></i> <br /> You're Not Verified </div>`;
     }
-    input.value = "";
+    
 }
